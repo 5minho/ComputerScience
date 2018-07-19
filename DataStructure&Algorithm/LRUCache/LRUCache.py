df9227ac815ae1da482c5eed922cb6c6a5282ad2
minomi <5minhominho@gmail.com>
2018-07-19 23:51:15 +0900

    LinkedList Node 구현

--- DataStructure&Algorithm/LRUCache/LRUCache.py
+++ DataStructure&Algorithm/LRUCache/LRUCache.py
@@ -8,28 +8,12 @@ class CacheMissError(Exception):
     pass
 
 
-class LinkedList:
-
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def insert_to_head(self, node):
-        pass
-
-    def move_to_head(self, node):
-        pass
-
-    def remove_from_tail(self):
-        pass
-
-
 class LRUCache:
 
     def __init__(self, size):
         self.size = size
-        self._list = LinkedList()
-        self._hash_table = {}
+        # self._list = LinkedList()
+        # self._hash_table = {}
 
     def get(self, item):
         pass
@@ -51,7 +35,6 @@ class LRUCacheTests(unittest.TestCase):
 
     def test_set(self):
         pass
-
 
 if __name__ == '__main__':
     unittest.main()
